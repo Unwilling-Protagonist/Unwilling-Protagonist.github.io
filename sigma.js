a639ec6dbbe5cdc250d5275a335a439b9fb7632e
Unwilling-Protagonist <trickaltorya@gmail.com>
2025-01-11 01:26:10 -0500

    i make thing that not done

---
+++ sigma.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+        const header = document.getElementById("header");
+    const page = document.body.getAttribute("class")
+    header.className = "slide-down"
+    if (page === "home"){
+        header.className  = "delayed-slide-down";
+    }
+    backgroundimage.className  = "slow-slide-down";
+    title.className = "even-slower-slide-down";
+    function hideHeader() {
+        if (window.scrollY > 1000) {
+            header.className = "slide-down";
+            header.style.position = "sticky";
+            header.style.top = "0";
+        } else {
+            header.style.position = "static";
+            header.className = "none";
+        }
+    }
+    window.addEventListener("scroll", hideHeader)
+});
